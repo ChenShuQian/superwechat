53f43a77cbf8ee4a2ba55e5ec1338ef9576b03c4
csa <861842668@qq.com>
2016-08-02 10:14:40 +0800

    新品Fragment布局

--- src/cn/ucai/fulicenter/activity/NewGoodsFragment.java
+++ src/cn/ucai/fulicenter/activity/NewGoodsFragment.java
@@ -1,30 +1,66 @@
 package cn.ucai.fulicenter.activity;
 
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.widget.SwipeRefreshLayout;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import cn.ucai.fulicenter.I;
 import cn.ucai.fulicenter.R;
+import cn.ucai.fulicenter.adapter.GoodAdapter;
+import cn.ucai.fulicenter.bean.NewGoodBean;
+import cn.ucai.fulicenter.bean.Result;
+import cn.ucai.fulicenter.utils.OkHttpUtils2;
+import cn.ucai.fulicenter.utils.Utils;
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class NewGoodsFragment extends Fragment {
+    SwipeRefreshLayout mSwipeRefreshLayout;
+    RecyclerView mRecyclerView;
+    List<NewGoodBean> mNewGoodList;
+    Context mContext;
 
-
+    GridLayoutManager mGridLayoutManager;
+    GoodAdapter mAdapter;
     public NewGoodsFragment() {
-        // Required empty public constructor
     }
-
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_new_goods, container, false);
+        mContext = getContext();
+        View view = inflater.inflate(R.layout.fragment_new_goods, container, false);
+        mNewGoodList = new ArrayList<>();
+        initView(view);
+        return view;
+    }
+
+    private void initView(View view) {
+        mSwipeRefreshLayout = (SwipeRefreshLayout) view.findViewById(R.id.srl_new_good);
+        mSwipeRefreshLayout.setColorSchemeColors(
+                R.color.google_blue,
+                R.color.google_yellow,
+                R.color.google_red,
+                R.color.google_green
+        );
+        mRecyclerView = (RecyclerView) view.findViewById(R.id.rvNewGood);
+        mGridLayoutManager = new GridLayoutManager(mContext, I.COLUM_NUM);
+        mGridLayoutManager.setOrientation(LinearLayout.VERTICAL);
+        mRecyclerView.setLayoutManager(mGridLayoutManager);
+        mAdapter = new GoodAdapter(mContext, mNewGoodList);
+        mRecyclerView.setAdapter(mAdapter);
     }
 
 }
